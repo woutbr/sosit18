f3bc791475d4655fb60c5b3ed32c46f32ef5401b
woutbr <c1043809@student.hik.be>
2018-11-24 15:02:24 +0100

    Elke startup zullen de Bootsfaces components geregistreerd worden. Methode getDefaultRenderKit gekopieerd uit org.primefaces.mobile.renderkit.MobileRenderKit @ManagedBean veranderd naar @Named omwille van nieuwere CDI. Methode startup toegevoegd welke de startup van de application observed.

--- Sosit18/src/java/controller/InitBootsfacesBean.java
+++ Sosit18/src/java/controller/InitBootsfacesBean.java
@@ -1,27 +1,41 @@
 package controller;
+
 import static com.sun.xml.ws.spi.db.BindingContextFactory.LOGGER;
 import java.util.logging.Level;
 import javax.enterprise.context.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.faces.FactoryFinder;
+import javax.inject.Named;
 import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.render.RenderKit;
+import javax.faces.render.RenderKitFactory;
 import javax.faces.render.Renderer;
 import net.bootsfaces.component.ComponentsEnum;
 
-@ManagedBean(eager=true)
 @ApplicationScoped
+@Named
 public class InitBootsfacesBean {
+
+    private RenderKit defaultRenderKit = null;
+
+    public void startup(@Observes @Initialized(ApplicationScoped.class) Object context) {
+        initializeBootsfacesRenderers();
+    }
+
     /**
      * Register the Bootsfaces renderers
      * https://beyondjava.net/running-bootsfaces-on-glassfish-or-payara
      */
     private void initializeBootsfacesRenderers() {
-        
+
         // Loop through all of the Bootsfaces components
         for (ComponentsEnum value : ComponentsEnum.values()) {
-            
+
             // Get the component class information
             String className;
-            
+
             // switchComponent has wrong classpath in ComponentsEnum
             if (value.name().equals("switchComponent")) {
                 // Use correct qualified name
@@ -30,16 +44,16 @@ public class InitBootsfacesBean {
                 // Otherwise, use specified value
                 className = value.classname();
             }
-            
+
             // See if this is an internal reference
             if (className.contains("Internal")) {
-                LOGGER.log(Level.INFO, "Init Bootsfaces: Skipping internal component: " + className);
+                LOGGER.log(Level.INFO, "Init Bootsfaces: Skipping internal component: {0}", className);
             } else {
-                
-                LOGGER.log(Level.INFO, "Init Bootsfaces: Processing component: " + className);
-                
+
+                LOGGER.log(Level.INFO, "Init Bootsfaces: Processing component: {0}", className);
+
                 try {
-                     // See if we can instantiate the class
+                    // See if we can instantiate the class
                     Class componentClass = Class.forName(className);
                     Class<UIComponentBase> baseClass = componentClass.asSubclass(UIComponentBase.class);
                     UIComponentBase component = baseClass.newInstance();
@@ -58,23 +72,35 @@ public class InitBootsfacesBean {
                             // We have to guess at the name of the renderer
                             rendererClassName = className + "Renderer";
                     }
-                    
+
                     // Look up the renderer
                     Class rendererSuperclass = Class.forName(rendererClassName);
                     Class<Renderer> rendererClass = rendererSuperclass.asSubclass(Renderer.class);
                     Renderer renderer = rendererClass.newInstance();
-                    LOGGER.log(Level.INFO, "Init Bootsfaces: Registering renderer: " + rendererFamily + "/" + rendererType);
+                    LOGGER.log(Level.INFO, "Init Bootsfaces: Registering renderer: {0}/{1}", new Object[]{rendererFamily, rendererType});
 
                     // ****** THIS IS THE IMPORTANT CALL TO REGISTER THE RENDERER *********
                     addRenderer(rendererFamily, rendererType, renderer);
                 } catch (Throwable e) {
                     LOGGER.log(Level.WARNING, "Init Bootsfaces: Unable to register renderer for component: " + className, e);
                 }
-           }
+            }
         }
     }
 
     public void addRenderer(String family, String rendererType, Renderer renderer) {
         getDefaultRenderKit().addRenderer(family, rendererType, renderer);
+    }
+
+    /*
+     * From org.primefaces.mobile.renderkit.MobileRenderKit
+     */
+    private RenderKit getDefaultRenderKit() {
+        if (defaultRenderKit == null) {
+            RenderKitFactory renderKitFactory = (RenderKitFactory) FactoryFinder.getFactory(FactoryFinder.RENDER_KIT_FACTORY);
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            defaultRenderKit = renderKitFactory.getRenderKit(facesContext, RenderKitFactory.HTML_BASIC_RENDER_KIT);
+        }
+        return defaultRenderKit;
     }
 }
