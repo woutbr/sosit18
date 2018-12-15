e1721a0710cb1d459c307247afec717341e17b19
woutbr <c1043809@student.hik.be>
2018-12-15 19:25:23 +0100

    Werk aan navbar

--- Sosit18/src/java/controller/NavBarBean.java
+++ Sosit18/src/java/controller/NavBarBean.java
@@ -1,9 +1,11 @@
 package controller;
 
+import be.hbo5.java.menu.HeaderNavBarLinks;
 import be.hbo5.java.menu.MenuItem;
 import be.hbo5.java.menu.MenuLink;
 import be.hbo5.java.menu.MenuList;
 import be.hbo5.java.xml.LinksXmlReader;
+import com.sun.el.ValueExpressionLiteral;
 import java.io.Serializable;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
@@ -27,56 +29,59 @@ import net.bootsfaces.component.navLink.NavLink;
 public class NavBarBean implements Serializable {
 
     private final MenuList links;
-    private NavBarLinks navBarMenu;
+//    private HeaderNavBarLinks navBarMenu;
 
     public NavBarBean() {
         ExternalContext ext = FacesContext.getCurrentInstance().getExternalContext();
         String linksXmlPath = ext.getRealPath("/templates/navbarlinks.xml");
         this.links = LinksXmlReader.readLinksFromXmlFile(linksXmlPath);
-        this.renderNavBarMenu();
+//        this.renderNavBarMenu();
     }
 
     public MenuList getLinks() {
         return links;
     }
 
-    public NavBarLinks getNavBarMenu() {
-        return navBarMenu;
-    }
+//    public NavBarLinks getNavBarMenu() {
+//        return navBarMenu;
+//    }
 
     /**
      * Create a BootsFaces NavBarLinks from the links in the MenuList variable.
      */
-    public final void renderNavBarMenu() {
-        this.navBarMenu = new NavBarLinks();
-        List<UIComponent> children = this.navBarMenu.getChildren();
-        for (MenuItem mi : this.links) {
-            if (MenuLink.class.isInstance(mi)) {
-                children.add(createNavLinkFromMenuLink((MenuLink) mi));
-            } else if (MenuList.class.isInstance(mi)) {
-                children.add(createDropMenuFromMenuList((MenuList) mi));
-            }
-        }
-    }
+//    public final void renderNavBarMenu() {
+//        this.navBarMenu = new HeaderNavBarLinks(this.links);
+//        List<UIComponent> children = this.navBarMenu.getChildren();
+//        for (MenuItem mi : this.links) {
+//            if (MenuLink.class.isInstance(mi)) {
+//                children.add(createNavLinkFromMenuLink((MenuLink) mi));
+//            } else if (MenuList.class.isInstance(mi)) {
+//                children.add(createDropMenuFromMenuList((MenuList) mi));
+//            }
+//        }
+//    }
 
-    private NavLink createNavLinkFromMenuLink(MenuLink menulink) {
-        NavLink navLink = new NavLink();
-        navLink.setValue(menulink.getName());
-        navLink.setHref(menulink.getHref());
-        return navLink;
-    }
-
-    private DropMenu createDropMenuFromMenuList(MenuList menulist) {
-        DropMenu dropmenu = new DropMenu();
-        List<UIComponent> children = dropmenu.getChildren();
-        for (MenuItem mi : menulist) {
-            if (MenuLink.class.isInstance(mi)) {
-                children.add(createNavLinkFromMenuLink((MenuLink) mi));
-            } else if (MenuList.class.isInstance(mi)) {
-                children.add(createDropMenuFromMenuList((MenuList) mi));
-            }
-        }
-        return dropmenu;
-    }
+//    private NavLink createNavLinkFromMenuLink(MenuLink menulink) {
+//        NavLink navLink = new NavLink();
+//        navLink.setValue(menulink.getName());
+//        navLink.setOutcome(menulink.getHref());
+//        System.out.println(navLink);
+//        return navLink;
+//    }
+//
+//    private DropMenu createDropMenuFromMenuList(MenuList menulist) {
+//        DropMenu dropmenu = new DropMenu();
+//        dropmenu.setValueExpression("value", new ValueExpressionLiteral(menulist.getName(), String.class));
+//        List<UIComponent> children = dropmenu.getChildren();
+//        for (MenuItem mi : menulist) {
+//            if (MenuLink.class.isInstance(mi)) {
+//                children.add(createNavLinkFromMenuLink((MenuLink) mi));
+//            } else if (MenuList.class.isInstance(mi)) {
+//                children.add(createDropMenuFromMenuList((MenuList) mi));
+//            }
+//        }
+//        System.out.println(dropmenu);
+//        return dropmenu;
+//    }
 
 }
