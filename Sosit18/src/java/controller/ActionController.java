5ee27888748aea87337c807e7c1cb6f8ab877d75
c1041184 <c1041184@G-D103-04.local.thomasmore.be>
2019-01-12 13:10:31 +0100

    Nog probleem met datum in action

--- Sosit18/src/java/controller/ActionController.java
+++ Sosit18/src/java/controller/ActionController.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 import java.sql.Date;
 import javax.ejb.EJB;
 import java.util.List;
+import javax.inject.Inject;
 
 
 /**
@@ -31,6 +32,9 @@ public class ActionController implements Serializable {
     private ActionFacade actionFacade;
     @EJB
     private TicketFacade ticketFacade;
+    
+    @Inject
+    private TicketController ticketController;
     
     private Action action = new Action();
     private Ticket ticket = new Ticket();
@@ -51,13 +55,18 @@ public class ActionController implements Serializable {
     }
        
     public String create(){
-        this.actionFacade.create(action);
-        action = new Action();
-        //return "action?faces-redirect=true";
+        if (this.action.getActionid()==null) {
+            this.actionFacade.create(action);
+            resetAction();
+        }else{
+            this.actionFacade.edit(action);
+        }
+   
         return "ticketList?faces-redirect=true";
     }
     
     public void onload(BigDecimal ticketid){
+
         LocalDate date = LocalDate.now();
         Date today = Date.valueOf(date);
         action.setCreationdate(today);
@@ -65,9 +74,30 @@ public class ActionController implements Serializable {
         
     }
     
+        public void onload2(BigDecimal actionId){
+            if (actionId!=null) {
+                this.action=this.actionFacade.FindById(actionId);
+                int a = 1 ; 
+            }else{
+                resetAction();
+                LocalDate date = LocalDate.now();
+                Date today = Date.valueOf(date);
+                action.setCreationdate(today);
+                BigDecimal ticketid= ticketController.getTicket().getTicketid();
+                action.setTicketid(this.ticketFacade.FindById(ticketid));
+            }
+   
+    }
+    
     public List<Action> GetAllActions(){
         return this.actionFacade.GetAllActions();
 
+    }
+    
+    public List<Action> listAllActionsByTicket(BigDecimal id){
+        
+        List<Action> l = this.actionFacade.listActionsByTicketId(id);
+        return l;        
     }
     
     public String cancel(){
